02a495c98acd7b3f61e64c3855440e82939ee101
Hannia Escamilla Pérez <hanescper@gmail.com>
2025-10-13 18:13:06 -0600

    Creating MST.h
    Draft of MST code

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 #include <vector>
 #include "./headers/tsp_loader.h"
-//#include "./headers/NN.h"
-//#include "./headers/CheapestLink.h"
-//#include "./headers/MST.h"
+// #include "./headers/NN.h"
+// #include "./headers/CheapestLink.h"
+// #include "./headers/MST.h"
 
 using namespace std;
 
-int main(){
-    try{
+int main()
+{
+    try
+    {
         vector<City> total_cities = loadTSP("dsj1000.txt");
         cout << "Loaded " << total_cities.size() << endl;
 
-        if (total_cities.size() >= 2) {
+        if (total_cities.size() >= 2)
+        {
             cout << "Distance between first two: "
-                      << euclidean(total_cities[0], total_cities[1]) << "\n";
+                 << euclidean(total_cities[0], total_cities[1]) << "\n";
         }
-    } catch(const exception& e){
+    }
+    catch (const exception &e)
+    {
         cerr << e.what() << '\n';
     }
 
@@ -26,10 +31,9 @@ int main(){
     vector<City> cities(number_of_cities - 1);
     // funcion random de n ciudades para guardarlas en cities no repetidas
 
-    //cout << "Nearest Neighbor: " << NearestNeighbor(cities) << endl;
-    //cout << "Cheapest Link: " << CheapestLink(cities) << endl;
-    //cout << "Minimum Spanning Tree: " << MST(cities) << endl;
-
+    // cout << "Nearest Neighbor: " << NearestNeighbor(cities) << endl;
+    // cout << "Cheapest Link: " << CheapestLink(cities) << endl;
+    // cout << "Minimum Spanning Tree: " << MST(cities) << endl;
 
     return 0;
 }
